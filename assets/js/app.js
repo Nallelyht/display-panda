e9218e4cb5cd14bbbbdae17e18eed677a618e88f
Nallelyht <you@example.com>
2017-04-22 13:26:05 -0500

    funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,29 @@ var longitud = imagen.length;
 var texto1 = document.getElementById("lorem1");
 var texto2 = document.getElementById("lorem2");
 var origen = document.getElementById("origen");
+var extincion = document.getElementById("extincion");
 
 
 restaurar.addEventListener("click",restaurarImagen);
+origen.addEventListener("click", ocultarOrigen);
+origen.addEventListener("dblclick", mostrarOrigen);
+extincion.addEventListener("click", ocultarExtincion);
+extincion.addEventListener("dblclick", mostrarExtincion);
 
+function mostrarExtincion(){
+    texto2.style.visibility="visible";
+}
+
+function ocultarExtincion(){
+    texto2.style.visibility="hidden";
+}
+function mostrarOrigen(){
+    texto1.style.visibility="visible";
+}
+
+function ocultarOrigen(){
+    texto1.style.visibility="hidden";
+}
 for (var i = 0; i < cerrar.length; i++){
 		cerrar[i].addEventListener("click",cerrarImagen);
 	}
@@ -17,13 +36,13 @@ function restaurarImagen(){
 	
 	
 	for (var i = 0; i < longitud; i++){
-		cerrar[i].style.visibility = "visible";
+		cerrar[i].style.display = "inline-block";
 	}
 	console.log(longitud);
 	console.log(imagen);
 }
 
 function cerrarImagen(){
-		this.style.visibility = "hidden";
+		this.style.display = "none";
 	
 }
